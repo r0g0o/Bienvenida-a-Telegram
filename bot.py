5136931c05a4fdaf4561719263a8243d6749b1ce
r0g0o <53545808+r0g0o@users.noreply.github.com>
2019-09-22 17:45:53 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ def cinco(message):
     bot.send_message(chatid, "Estos son seguros y pueden hacer infinidad de cosas, por ejemplo, @getmediabot descarga cualquier cancion que le escribamos, @memerator_bot genera memes simples sólo a partir de una imagen, como la siguiente")
 
 
-    meme1=open('/Users/fncg6/OneDrive/Escritorio/pruebas programar/Bot Telegram/temp/meme.png','rb')
+    meme1=open('/temp/meme.png','rb')
     bot.send_photo(chatid, meme1)
 
     bot.send_message(chatid, "Un par de últimas cosas. /Dime")
