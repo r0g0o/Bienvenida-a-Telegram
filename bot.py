dc66b9627e846b5236a543608f08b760c2bee774
r0g0o <53545808+r0g0o@users.noreply.github.com>
2019-09-23 01:58:01 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import telebot
-import Flask, request
-import os
+#import Flask, request
+#import os
 
 bot = telebot.TeleBot("872259319:AAHPKA8Csh6ERD0RMLN90zZZPzluFqoFI88")
-server = Flask(__name__)
+#server = Flask(__name__)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -89,7 +89,7 @@ def send_photo(message):
 
 print("Ejecutandose")
 
-
+"""
 @server.route('/' + "872259319:AAHPKA8Csh6ERD0RMLN90zZZPzluFqoFI88", methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
@@ -105,7 +105,7 @@ def webhook():
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-
+"""
     
 bot.polling()
 
