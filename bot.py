16617cc1150a5ce6bcd8e747dcc2abc5a37a5dcb
r0g0o <53545808+r0g0o@users.noreply.github.com>
2019-09-23 00:51:00 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -97,7 +97,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://your_heroku_project.com/' + TOKEN)
+    bot.set_webhook(url='https://bienvenidatelegram.herokuapp.com/' + TOKEN)
     return "!", 200
 
 
