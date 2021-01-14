bdb316902755ebc6d3665ae5aa8d26f6acfeed44
Rodrigo Castillo <53545808+r0g0o@users.noreply.github.com>
2021-01-14 01:12:55 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,28 +12,10 @@ TOKEN = '872259319:AAHPKA8Csh6ERD0RMLN90zZZPzluFqoFI88'
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
-
 def start(update, context):
-    update.message.reply_text('Hola, mundo')
-    update.message.reply_text('Hola, /clikeame')
-'''
-def uno(update, context):
-    update.message.reply_text('En primer primer lugar, aca también sí hay stickers y están más ordenados. Agregalos!')
-    update.message.reply_text('https://t.me/addstickers/sebaspack')
-    update.message.reply_text('https://t.me/addstickers/Meme_stickers')
-    update.message.reply_text('https://t.me/addstickers/CDJMeme')
-    update.message.reply_text('/continua')
-    
-def dos(update, context):
-    update.message.reply_text('Incluso hay stickers animados')
-    update.message.reply_text('Intenta mandar el emoji de un corazón')
-    
-def tres(update, context):
-    update.message.reply_text('Muy bien, luego investiga el resto de emojis que son animados. Mientas, explora algunos packs de stickers animados')
-    update.message.reply_text('https://t.me/addstickers/HotCherry')
-    update.message.reply_text('https://t.me/addstickers/CorgiMuffin')
-    update.message.reply_text('/excelente !!')
-'''
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi world!')
+    update.message.reply_text('XD')
 
 def help(update, context):
     """Send a message when the command /help is issued."""
@@ -58,10 +40,9 @@ def main():
     dp = updater.dispatcher
 
     # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))"""
-    dp.add_handler(CommandHandler("clickeame", uno))
-    dp.add_handler(CommandHandler("continua", dos))
-    dp.add_handler(CommandHandler("❤", tres))"""
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
