2c9b8a3f6ea1616f160533ff8131a053b2e19111
Rodrigo Castillo <53545808+r0g0o@users.noreply.github.com>
2021-01-14 00:34:51 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,111 +1,67 @@
-import telebot
-#import Flask, request
-#import os
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
+PORT = int(os.environ.get('PORT', 5000))
 
-bot = telebot.TeleBot("872259319:AAHPKA8Csh6ERD0RMLN90zZZPzluFqoFI88")
-#server = Flask(__name__)
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    chatid=message.chat.id
+logger = logging.getLogger(__name__)
+TOKEN = '872259319:AAHPKA8Csh6ERD0RMLN90zZZPzluFqoFI88'
 
-    bot.send_message(chatid, "Hola, /clikeame")
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
 
-@bot.message_handler(commands=['clikeame'])
-def uno(message):
-    chatid=message.chat.id
-        
-    bot.send_message(chatid, "En primer primer lugar, aca también sí hay stickers y están más ordenados. Agregalos!")
-    bot.send_message(chatid, "https://t.me/addstickers/sebaspack")
-    bot.send_message(chatid, "https://t.me/addstickers/Meme_stickers")
-    bot.send_message(chatid, "https://t.me/addstickers/CDJMeme")
-    bot.send_message(chatid, "/continua")
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
 
-@bot.message_handler(commands=['continua'])
-def dos(message):
-    chatid=message.chat.id
-        
-    bot.send_message(chatid, "Incluso hay stickers animados")
-    bot.send_message(chatid, "Intenta mandar el emoji de un corazón")
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
 
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-@bot.message_handler(regexp="❤")
-def tres(message):
-    chatid=message.chat.id
-    bot.send_message(chatid, "Muy bien, ahora investiga el resto de emojis que son animados. Mientas, explora algunos packs de stickers animados")
-    bot.send_message(chatid, "https://t.me/addstickers/HotCherry")
-    bot.send_message(chatid, "https://t.me/addstickers/CorgiMuffin")
-    bot.send_message(chatid, "/excelente !!")
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
 
-@bot.message_handler(commands=['excelente'])
-def cuatro(message):
-    chatid=message.chat.id
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
 
-    bot.send_message(chatid, "Pues existen los grupos y los canales")
-    bot.send_message(chatid, "En los grupos cualquiera puede hablar, como en @excelhechofacil")
-    bot.send_message(chatid, "Mientras que en los canales sólo recibes los mensajes, como en @zukulentosmemes")
-    bot.send_message(chatid, "Para buscar más grupos y canales entra a @ListaTelegram, @listadogram o @DirectorioTelegram, donde están filtrados por tópico")
-    bot.send_message(chatid, "/genial!")
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
 
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
 
-@bot.message_handler(commands=['genial'])
-def cinco(message):
-    chatid=message.chat.id
+    # log all errors
+    dp.add_error_handler(error)
 
-    bot.send_message(chatid, "Tambien existen los bots (como el que estás hablando ahora).")
-    bot.send_message(chatid, "Estos son seguros y pueden hacer infinidad de cosas, por ejemplo, @getmediabot descarga cualquier cancion que le escribamos, @memerator_bot genera memes simples sólo a partir de una imagen, como la siguiente")
+    # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://bienvenidatelegram.herokuapp.com/' + TOKEN)
 
-        
-    bot.send_photo(chatid, open( './temp/meme.png', 'rb'))
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
 
-    bot.send_message(chatid, "Un par de últimas cosas. /Dime")
-
-@bot.message_handler(commands=['Dime'])
-def seis(message):
-    chatid=message.chat.id
-
-    bot.send_message(chatid, "Todo el contenido que recibas está en una nube y puedes siempre volver a descargarlo en caso de borrarlo")
-    bot.send_message(chatid, "Para borrar el caché sigue el siguiente tutorial")
-
-    bot.send_video(chatid, open( './temp/vidcache.mp4', 'rb'))    
-    
-    bot.send_message(chatid, "Y por último, se pueden guardar gifs, mira este pequeño tutorial :3")
-
-    bot.send_video(chatid, open( './temp/vidgif.mp4', 'rb'))
-
-    bot.send_message(chatid, "Intenta!")
-
-    bot.send_video(chatid, open( './temp/fry.mp4', 'rb'))
-
-    bot.send_message(chatid, "Y eso es todo, bienvenido oficialmente a Telegram")
-
-@bot.message_handler(commands=['foto'])
-def send_photo(message):
-    chatid=message.chat.id
-        
-    bot.send_message(chatid, "wolas ")
-
-    bot.send_photo(chatid, open( './temp/hola.png', 'rb'))
-
-print("Ejecutandose")
-
-"""
-@server.route('/' + "872259319:AAHPKA8Csh6ERD0RMLN90zZZPzluFqoFI88", methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+if __name__ == '__main__':
+    main()
 
 
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://bienvenidatelegram.herokuapp.com/' + "872259319:AAHPKA8Csh6ERD0RMLN90zZZPzluFqoFI88")
-    return "!", 200
 
-
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-"""
-    
-bot.polling()
 
